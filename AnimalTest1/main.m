b5d47b8311486557de09c61418b7b2b7301550fe
VincentWe <272035026@qq.com>
2016-12-26 13:33:49 +0800

    Initial Commit

---
+++ AnimalTest1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnimalTest1
+//
+//  Created by VincentWe on 2016/12/26.
+//  Copyright © 2016年 VincentWe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
